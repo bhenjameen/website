a412c8501984512627ac81aecdb2afa33c661b8e
Benjamin Patrick <61899689+bhenjameen@users.noreply.github.com>
2020-11-03 16:34:14 +0100

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,68 @@
+//new WOW().init();
+
+function team() {
+    var dots = document.getElementById("dots");
+    var more = document.getElementById("more");
+    var btntext = document.getElementById("teambtn");
+
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btntext.innerHTML = "Read More";
+        more.style.display = "none";
+    } else {
+        dots.style.display = "none";
+        btntext.innerHTML = "Read Less";
+        more.style.display = 'inline';
+    }
+}
+
+
+function team0() {
+    var dots0 = document.getElementById("dots0");
+    var more0 = document.getElementById("more0");
+    var btntext0 = document.getElementById("teambtn0");
+
+    if (dots0.style.display === "none") {
+        dots0.style.display = "inline";
+        btntext0.innerHTML = "Read More";
+        more0.style.display = "none";
+    } else {
+        dots0.style.display = "none";
+        btntext0.innerHTML = "Read Less";
+        more0.style.display = 'inline';
+    }
+}
+
+
+function team1() {
+    var dots1 = document.getElementById("dots1");
+    var more1 = document.getElementById("more1");
+    var btntext1 = document.getElementById("teambtn1");
+
+    if (dots1.style.display === "none") {
+        dots1.style.display = "inline";
+        btntext1.innerHTML = "Read More";
+        more1.style.display = "none";
+    } else {
+        dots1.style.display = "none";
+        btntext1.innerHTML = "Read Less";
+        more1.style.display = 'inline';
+    }
+}
+
+
+function team2() {
+    var dots2 = document.getElementById("dots2");
+    var more2 = document.getElementById("more2");
+    var btntext2 = document.getElementById("teambtn2");
+
+    if (dots2.style.display === "none") {
+        dots2.style.display = "inline";
+        btntext2.innerHTML = "Read More";
+        more2.style.display = "none";
+    } else {
+        dots2.style.display = "none";
+        btntext2.innerHTML = "Read Less";
+        more2.style.display = 'inline';
+    }
+}
